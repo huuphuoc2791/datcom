d18a4752f64304d2be76ebbdd9b1553c2eabde08
HuuPhuoc <huuphuoc2791@gmail.com>
2016-10-15 09:17:17 +0700

    split word to send sms

--- common/CommonFunction.php
+++ common/CommonFunction.php
@@ -62,26 +62,55 @@ class CommonFunction
 
     }
 
-    public static function guid($hasHyphens = true,$hasBraces = false) {
-        if (function_exists('com_create_guid')){
+    public static function guid($hasHyphens = true, $hasBraces = false)
+    {
+        if (function_exists('com_create_guid')) {
             return com_create_guid();
-        }else{
-            mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        } else {
+            mt_srand((double)microtime() * 10000);//optional for php 4.2.0 and up.
             $charid = strtoupper(md5(uniqid(rand(), true)));
-            $hyphen = $hasHyphens?chr(45):'';// "-"
-            $braceStart = $hasBraces?chr(123):'';
-            $braceEnd = $hasBraces?chr(125):'';
+            $hyphen = $hasHyphens ? chr(45) : '';// "-"
+            $braceStart = $hasBraces ? chr(123) : '';
+            $braceEnd = $hasBraces ? chr(125) : '';
             if ($hasHyphens == false) {
                 $hyphen = '';
             }
             $uuid = $braceStart// "{"
-                .substr($charid, 0, 8).$hyphen
-                .substr($charid, 8, 4).$hyphen
-                .substr($charid,12, 4).$hyphen
-                .substr($charid,16, 4).$hyphen
-                .substr($charid,20,12)
-                .$braceEnd;// "}"
+                . substr($charid, 0, 8) . $hyphen
+                . substr($charid, 8, 4) . $hyphen
+                . substr($charid, 12, 4) . $hyphen
+                . substr($charid, 16, 4) . $hyphen
+                . substr($charid, 20, 12)
+                . $braceEnd;// "}"
             return $uuid;
         }
+    }
+
+    public static function convert_vi_to_en($str)
+    {
+        $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+        $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+        $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+        $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+        $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+        $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+        $str = preg_replace("/(đ)/", 'd', $str);
+        $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+        $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+        $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+        $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+        $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+        $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+        $str = preg_replace("/(Đ)/", 'D', $str);
+        return $str;
+
+    }
+
+    public static function splitWordToSMS($str)
+    {
+        $convertViToEN = self::convert_vi_to_en($str);
+        $totalOfWord = explode(' ', $convertViToEN);
+        $result =$totalOfWord[0]." ".$totalOfWord[1]." ".$totalOfWord[2];
+        return $result;
     }
 }
