05b18780bf7ac070566d608ce5bc8af275b392de
HuuPhuoc <huuphuoc2791@gmail.com>
2016-11-28 14:11:08 +0700

    new branch sandbox

--- common/constants.php
+++ common/constants.php
@@ -1,4 +1,5 @@
 <?php
 //define('ROOT_URL','http://local.datcom'); //loc
-define('ROOT_URL','http://sandbox-datcom.ple-server.me'); //production
+//define('ROOT_URL','http://sandbox-datcom.ple-server.me'); //production
+define('ROOT_URL','http://localhost/datcom'); //phuoc 
 define('Production_Version','20161128');
